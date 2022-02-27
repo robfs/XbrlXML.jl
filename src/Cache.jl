b5305e1e10fdf8a5c52b27f97672013f9b04af7e
Rob <rsimms1990@gmail.com>
2022-02-27 15:45:40 +0000

    refactor Cache.jl

--- src/Cache.jl
+++ src/Cache.jl
@@ -16,28 +16,21 @@ mutable struct HttpCache
 
 end
 
-
-    HttpCache() = new("./cache/", Dict())
-
-
-HttpCache(cache_dir::AbstractString, headers::Vector{Pair{AbstractString,AbstractString}}) = HttpCache(cache_dir, Dict(headers))
-
 function cache_file(cache::HttpCache, file_url::AbstractString)::AbstractString
-    
+
     file_path::AbstractString = url_to_path(cache, file_url)
-    
+
     isfile(file_path) && return file_path
-    
+
     file_dir_path::AbstractString = join(split(file_path, "/")[1:end-1], "/")
-    
+
     mkpath(file_dir_path)
-    
+
     Downloads.download(file_url, file_path; headers=cache.headers)
 
     return file_path
 
 end
-
 
 function purge_file(cache::HttpCache, file_url::AbstractString)::Bool
     try
@@ -48,37 +41,32 @@ function purge_file(cache::HttpCache, file_url::AbstractString)::Bool
     return true
 end
 
-
 function url_to_path(cache::HttpCache, url::AbstractString)::AbstractString
     rep::Pair{Regex, AbstractString} = r"https?://" => ""
     return cache.cache_dir * replace(url, rep)
 end
 
-
 function cache_edgar_enclosure(cache::HttpCache, enclosure_url::AbstractString)
-    
+
     if endswith(enclosure_url, ".zip")
-        
+
         enclosure_path::AbstractString = cache_file(cache, enclosure_url)
-        
+
         parent_path::AbstractString = join(split(enclosure_url, "/")[1:end-1], "/")
-        
+
         submission_dir_path::AbstractString = url_to_path(cache, parent_path)
-        
+
         r::ZipFile.Reader = ZipFile.Reader(enclosure_path)
-        
+
         for f in r.files
             write("$(submission_dir_path)/$(f.name)", read(f, String))
         end
-        
+
         close(r)
 
     else
         throw(error("This is not a valid zip folder"))
     end
 end
-
-
-
 
 end # Module
